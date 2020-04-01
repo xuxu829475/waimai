7b799579e4bb54a804151a52f8cdf59a58287afb
xuxu <8023xu829475@gmail.com>
2020-04-02 00:03:03 +0800

    搜索页面集成

--- waimai-parent/waimai-service/waimai-service-goods/src/main/java/com/waimai/goods/GoodsApplication.java
+++ waimai-parent/waimai-service/waimai-service-goods/src/main/java/com/waimai/goods/GoodsApplication.java
@@ -4,6 +4,7 @@ import entity.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -16,6 +17,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan(basePackages = {"com.waimai.goods.dao"})
+@EnableFeignClients
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
